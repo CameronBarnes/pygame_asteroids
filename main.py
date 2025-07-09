00011fdd21957da29878c31ac7deb45ea8438397
Cameron Barnes <cameron_barnes@outlook.com>
2025-07-08 23:57:33 -0700

    Created basic shape class to base everything on. Created player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
 	print("Starting Asteroids!")
@@ -9,14 +10,19 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+	dt = 0.0
 	running = True
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
